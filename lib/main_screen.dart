3d1f05389d32aee6c836f59b550ffc54af67553f
drmiltonc <92145221+drmiltonc@users.noreply.github.com>
2024-03-20 03:46:59 -0500

    rout implement

---
+++ lib/main_screen.dart
@@ -0,0 +1,71 @@
+import 'package:dashboard_ui_responsive/account_settings.dart';
+import 'package:dashboard_ui_responsive/analytics.dart';
+import 'package:dashboard_ui_responsive/cart.dart';
+import 'package:dashboard_ui_responsive/orders.dart';
+import 'package:dashboard_ui_responsive/profile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'logout.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({
+    super.key,
+  });
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  int screenIndex = 0;
+  late bool showNavigationDrawer;
+
+  void handleScreenChanged(int selectedScreen) {
+    setState(() {
+      screenIndex = selectedScreen;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
+      ),
+      drawer: NavigationDrawer(
+          selectedIndex: screenIndex,
+          onDestinationSelected: (int index) {
+            setState(() {
+              screenIndex = index;
+            });
+          },
+          children: const [
+            DrawerHeader(child: Center(child: Text('name@gmail.com'))),
+            NavigationDrawerDestination(
+                icon: Icon(Icons.person_2_rounded), label: Text('Profile')),
+            NavigationDrawerDestination(
+                icon: Icon(Icons.settings), label: Text('Account Settings')),
+            NavigationDrawerDestination(
+                icon: Icon(Icons.shopping_cart_checkout_rounded),
+                label: Text('Cart')),
+            NavigationDrawerDestination(
+                icon: Icon(Icons.list_alt_rounded), label: Text('Orders')),
+            NavigationDrawerDestination(
+                icon: Icon(Icons.analytics_rounded), label: Text('Analytics')),
+            NavigationDrawerDestination(
+                icon: Icon(Icons.logout_rounded), label: Text('Logout')),
+          ]),
+      body: const [
+        Profile(),
+        AccountSettings(),
+        Cart(),
+        Orders(),
+        Analytics(),
+        Logout(),
+      ][screenIndex],
+    );
+  }
+}
